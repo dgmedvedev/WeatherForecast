3c689b625d535c981fc18b6b96c13326cc8c11d9
dgmedvedev <dg-medvedev@mail.ru>
2023-03-10 13:21:12 +0300

    add class DownloadContent

--- app/src/main/java/com/demo/weatherforecast/MainActivity.java
+++ app/src/main/java/com/demo/weatherforecast/MainActivity.java
@@ -2,8 +2,10 @@ package com.demo.weatherforecast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -11,41 +13,46 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
 
     String address;
-    String data = "";
-    StringBuilder result = new StringBuilder();
+    Button buttonDownload;
+    TextView textViewWeather;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        address = "http://api.openweathermap.org/data/2.5/weather?q=Minsk&appid=85f1361d760adf6d851676c22ea6f89f";
+        address = getString(R.string.url_address);
+        buttonDownload = findViewById(R.id.buttonDownload);
+        textViewWeather = findViewById(R.id.textViewWeather);
 
-        getContent();
-
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-
-        Log.i("DATA_WEATHER3", data);
+        buttonDownload.setOnClickListener(view -> {
+            String text = getContent();
+            textViewWeather.setText(text);
+        });
     }
 
-    private void getContent() {
-        ExecutorService executor = Executors.newSingleThreadExecutor();
+    private String getContent() {
+        DownloadContent task = new DownloadContent();
+        try {
+            return task.execute(address).get();
+        } catch (ExecutionException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-        executor.execute(() -> {
+    private static class DownloadContent extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... strings) {
             URL url;
             HttpURLConnection urlConnection = null;
+            StringBuilder result = new StringBuilder();
             try {
-                url = new URL(address);
+                url = new URL(strings[0]);
                 urlConnection = (HttpURLConnection) url.openConnection();
                 InputStream inputStream = urlConnection.getInputStream();
                 InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
@@ -55,15 +62,15 @@ public class MainActivity extends AppCompatActivity {
                     result.append(line);
                     line = reader.readLine();
                 }
-                data = result.toString();
+                return result.toString();
             } catch (IOException e) {
-                throw new RuntimeException(e);
+                e.printStackTrace();
             } finally {
                 if (urlConnection != null) {
                     urlConnection.disconnect();
                 }
             }
-
-        });
+            return null;
+        }
     }
 }

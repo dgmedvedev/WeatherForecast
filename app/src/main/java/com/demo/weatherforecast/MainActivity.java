55ffd8fb10ac2cbdbdeb90d5107bf9dc7cf56b14
dgmedvedev <dg-medvedev@mail.ru>
2023-03-10 17:56:31 +0300

    add method getContent (working with JSON)

--- app/src/main/java/com/demo/weatherforecast/MainActivity.java
+++ app/src/main/java/com/demo/weatherforecast/MainActivity.java
@@ -2,10 +2,16 @@ package com.demo.weatherforecast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -13,11 +19,14 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class MainActivity extends AppCompatActivity {
 
-    String address;
+    String json;
+    String urlAddress;
+    Button buttonClear;
     Button buttonDownload;
     TextView textViewWeather;
 
@@ -26,33 +35,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        address = getString(R.string.url_address);
+        urlAddress = getString(R.string.url_address);
+        buttonClear = findViewById(R.id.buttonClear);
         buttonDownload = findViewById(R.id.buttonDownload);
         textViewWeather = findViewById(R.id.textViewWeather);
 
+        getContent();
+
+        buttonClear.setOnClickListener(view -> {
+            textViewWeather.setText("");
+        });
+
         buttonDownload.setOnClickListener(view -> {
-            String text = getContent();
-            textViewWeather.setText(text);
+            textViewWeather.setText(json);
         });
     }
 
-    private String getContent() {
-        DownloadContent task = new DownloadContent();
-        try {
-            return task.execute(address).get();
-        } catch (ExecutionException | InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-    }
+    private void getContent() {
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        Handler handler = new Handler(Looper.getMainLooper());
 
-    private static class DownloadContent extends AsyncTask<String, Void, String> {
-        @Override
-        protected String doInBackground(String... strings) {
+        executor.execute(() -> {
             URL url;
             HttpURLConnection urlConnection = null;
             StringBuilder result = new StringBuilder();
             try {
-                url = new URL(strings[0]);
+                url = new URL(urlAddress);
                 urlConnection = (HttpURLConnection) url.openConnection();
                 InputStream inputStream = urlConnection.getInputStream();
                 InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
@@ -62,7 +70,7 @@ public class MainActivity extends AppCompatActivity {
                     result.append(line);
                     line = reader.readLine();
                 }
-                return result.toString();
+                json = result.toString();
             } catch (IOException e) {
                 e.printStackTrace();
             } finally {
@@ -70,7 +78,23 @@ public class MainActivity extends AppCompatActivity {
                     urlConnection.disconnect();
                 }
             }
-            return null;
-        }
+            handler.post(() -> {
+                textViewWeather.setText(json);
+                try {
+                    JSONObject jsonObject = new JSONObject(json);
+                    String name = jsonObject.getString("name");
+
+                    JSONObject main = jsonObject.getJSONObject("main");
+                    String temp = main.getString("temp");
+
+                    JSONArray jsonArray = jsonObject.getJSONArray("weather");
+                    JSONObject weather = jsonArray.getJSONObject(0);
+                    String id = weather.getString("id");
+                    Log.i("CITY_ID", name + ", T = " + temp + ", id = " + id);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            });
+        });
     }
 }
